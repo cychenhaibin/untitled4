9abcff38738814554d1fb7bf9e28549148e52f60
cychenhaibin <3242477764@qq.com>
2022-11-14 23:28:37 +0800

    java

---
+++ src/Main.java
@@ -0,0 +1,401 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("请输入第一个正数");
+        int number1 = sc.nextInt();
+        System.out.println("请输入第二个正数");
+        int number2 = sc.nextInt();
+        System.out.println(number1+number2);
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);*/
+
+
+
+        /*System.out.println(3+2);
+        System.out.println(5-4);
+        System.out.println(7*9);
+        System.out.println(1.1-1.01);
+        System.out.println(1.1+1.01);
+        System.out.println(1.1*1.01);*/
+
+
+
+        /*System.out.println(10/2);
+        System.out.println(10/3);
+        System.out.println(10.0/3);
+        System.out.println(10%2);
+        System.out.println(10%3);
+        System.out.println();*/
+
+
+
+        //键盘录入
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个整数：");
+        int number = sc.nextInt();
+        int ge = number%10;
+        int shi = number/10%10;
+        int bai = number/100%10;
+        System.out.println(ge);
+        System.out.println(shi);
+        System.out.println(bai);*/
+
+
+
+        /*byte b1 = 100;
+        byte b2 = 20;
+        byte result = (byte)(b1 + b2);
+        System.out.println(result);
+        System.out.println(3.7+"abc");
+        System.out.println('中'+"abc"+true);
+        int age=18;
+        System.out.println("我的年龄是"+age+"岁"); //”我的年龄是18岁“
+        System.out.println("我的年龄是"+"age"+"岁"); //”我的年龄是age岁“
+        System.out.println(1+2+"age"+2+1); //3age21*/
+
+
+
+        //Scanner sc = new Scanner(System.in);
+        /*System.out.println("请输入一个整数：");
+        int number = sc.nextInt();
+        int ge = number%10;
+        int shi = number/10%10;
+        int bai = number/100%10;
+        System.out.println("个位是："+ge);
+        System.out.println("十位是："+shi);
+        System.out.println("百位是："+bai);*/
+
+
+
+        /*int a = 10;
+        a++;
+        System.out.println(a); //11
+        ++a;
+        System.out.println(a); //12
+        a--;
+        System.out.println(a); //11
+        --a;
+        System.out.println(a); //10*/
+
+
+
+        /*int a = 10;
+        int b = 20;
+        a+=b;
+        System.out.println(a); //30
+        System.out.println(b); //20*/
+
+
+
+        /*int a = 10;
+        int b = 10;
+        int c =20;
+        System.out.println(a==b);
+        System.out.println(a==c);
+        System.out.println(a!=c);
+        Scanner si=new Scanner(System.in);
+        System.out.println("请输入自己的：");
+        int myFashion = si.nextInt();
+        System.out.println("请输入对象的：");
+        int girlFashion = si.nextInt();
+        boolean result = myFashion>girlFashion;
+        System.out.println(result);*/
+
+
+
+        /*Scanner sc= new Scanner(System.in);
+        System.out.println("请输入女婿的酒量");
+        int wine=sc.nextInt();
+        if(wine>2) {
+            System.out.println("兄啊伙子不错");
+        }
+        else {
+            System.out.println("你好菜");*/
+
+
+
+        /*int ranking = 1;
+        if(ranking == 1){
+            System.out.println("小红成为小明的女朋友");*/
+
+
+
+        /*boolean isLightGreen = false;
+        boolean isLightYellow = false;
+        boolean isLightRed = true;
+        if(isLightGreen){
+            System.out.println("GoGoGo!!!");
+        }
+        if(isLightYellow){
+            System.out.println("slow");
+        }
+        if(isLightRed){
+            System.out.println("stop!!!");
+        }*/
+
+
+
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("请输入身上的钱");
+        int money = sc.nextInt();
+        if(money >= 100){
+            System.out.println("吃网红餐");
+        }else{
+            System.out.println("吃小吃");
+        }*/
+
+
+
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个整数表示实际支付的钱");
+        int money = sc.nextInt();
+        if(money>=600){
+            System.out.println("付款成功");
+        }else{
+            System.out.println("付款失败");
+        }*/
+
+
+
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("输入数字");
+        int ticket=sc.nextInt();
+        if(ticket>=0&&ticket<=100) {
+            if (ticket % 2 == 1) {
+                System.out.println("做左边");
+            } else {
+                System.out.println("做右边");
+            }
+        }*/
+
+
+
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个数");
+        int score = sc.nextInt();
+        if(score>0&&score<100) {
+            if (score >= 95 && score <= 100) {
+                System.out.println("送自行车");
+            } else if (score >= 90 && score <= 94) {
+                System.out.println("玩一天");
+            } else if (score >= 80 && score <= 89) {
+                System.out.println("送玩具");
+            } else {
+                System.out.println("爽一下");
+            }
+        }else{
+            System.out.println("当前输入的数字不合法");
+        }*/
+
+
+
+        /*int price=1000;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("会员级别");
+        int vip=sc.nextInt();
+        if(vip==1){
+            System.out.println("实际支付的钱为"+(price*0.9));
+        }else if(vip==2){
+            System.out.println("实际支付的钱为"+(price*0.8));
+        }else if(vip==3){
+            System.out.println("实际支付的钱为"+(price*0.7));
+        }else{
+            System.out.println("实际支付的钱为"+price);
+        }*/
+
+
+
+        /*String noodless = "兰州拉面";
+        switch(noodless){
+            case "兰州拉面":
+                System.out.println("吃兰州拉面");
+                break;
+            case "武汉人擀面":
+                System.out.println("武汉人擀面");
+                break;
+            case "北京杂酱面":
+                System.out.println("北京杂酱面");
+                break;
+            case "陕西面":
+                System.out.println("陕西面");
+                break;
+            default:
+                System.out.println("吃方便面");
+                break;
+        }*/
+
+
+
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("请输入星期");
+        int week = sc.nextInt();
+        switch(week){
+            case 1:
+                System.out.println("跑步");
+                break;
+            case 2:
+                System.out.println("游泳");
+                break;
+            case 3:
+                System.out.println("慢走");
+                break;
+            case 4:
+                System.out.println("骑车");
+                break;
+            case 5:
+                System.out.println("拳击");
+                break;
+            case 6:
+                System.out.println("爬山");
+                break;
+            case 7:
+                System.out.println("好好吃一顿");
+                break;
+            default:
+                System.out.println("没有这个星期");
+                break;*/
+
+
+
+        //打印1-5
+        /*for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+        //打印5-1
+        for(int i=5;i>=1;i--){
+            System.out.println(i);
+        }*/
+
+
+
+        /*int sum = 0;
+        for(int i=1;i<=100;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);*/
+
+
+
+        /*int sum = 0;
+        for(int i=0;i<=100;i+=2){
+            sum=sum+i;
+        }
+        System.out.println(sum);*/
+
+
+
+       /*Scanner sc=new Scanner(System.in);
+       System.out.println("请输入一个数字表示范围的开始");
+       int start = sc.nextInt();
+       System.out.println("请输入一个数字表示范围的结束");
+       int end = sc.nextInt();
+       int count=0;
+       for(int i=start;i<=end;i++){
+           if(i%3==0&&i%5==0){
+               System.out.println(i);
+               count++;
+               System.out.println(count);
+           }
+       }*/
+
+
+
+       /*double height=8844430;
+       double paper=0.1;
+       int count=0;
+       while(paper<=height){
+           paper*=2;
+           count++;
+       }
+       System.out.println(count);*/
+
+
+
+        //数组格式
+        //数据类型[] 数组名 = new 数据类型[]{元素1,元素2，。。。}
+        //简化模式
+        //数据类型[] 数组名 = {元素1，元素2，。。。}
+        //int[] arr1 = {1,12,13,14};
+        //String[] arr2 = {"张三","王五","李四"};
+        //double[] arr3 = {1.93,1.75,1.81,1.76};
+        //解释一下地址值的格式含义[D@776ecBdf
+        //[ :表示当前是一个数组
+        //D :表示当前数组里的类型都是double类型的
+        //@ :表示一个间隔符号(固定格式）
+        //776ecBdf :表示才是数组真正的地址值(十六进制)
+
+
+
+        //数组元素访问
+        //利用索引对数组中的元素进行访问
+        //1.获取数组里面的元素
+        //格式： 数组名[索引]
+        /*int[]arr = {1,2,3,4,5};
+        int number = arr[0];
+        System.out.println(number);
+        System.out.println(arr[1]);
+        //2.把数据储存到数组当中
+        //数组名[索引] = 具体数据/变量；
+        //细节： 一旦覆盖之后原来的数据就不存在了
+        arr[0] = 100;
+        System.out.println(arr[0]); */
+
+
+
+        //数组遍历
+        //1.定义数组
+        /*int[] arr = {1,2,3,4,5};
+        //for(int i=0;i<5;i++)
+        //{
+            //System.out.println(arr[i]);
+        //}
+        //java中，关于数组的一个长度属性，lenght
+        //调用方式，数组名.lenght
+        for(int i=0;i<arr.lenght;i++){
+            System.out.println(arr[i]);
+        }
+        //自动的快捷生成数组的方式：数组名.fori */
+
+
+
+        //遍历数组并求和
+        /*int[] arr = {1,2,3,4,5};
+        //求和变量
+        int sum = 0;
+        //2.遍历数组得到每一个数组，累加求和
+        for(int i=0;i<arr.length;i++){
+            //i依次表示数组里的每一个索引
+            //arr[i] 依次表示数组里面的每一个元素
+            sum = sum+arr[i];
+        }
+        System.out.println(sum); */
+
+
+
+        //格式：
+        //数据类型[] 数组名 = 呢哇数据类型[数组的长度]；
+        //在创建的时候，由我们自己指定数组的长度，由虚拟机给出默认的初始化值
+
+        //定义一个数组，用来存班级中50个学生的姓名，姓名未知，等学生报道后添加，再进行添加
+        String[] arr = new String[50];
+        //添加学生
+        arr[0]="张三";
+        arr[1]="李四";
+        //获取
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+        //数组默认初始化值的规律
+        //整数类型：默认初始化值为0；
+        //小数类型：默认初始化值为0.0；
+        //字符串类型：默认初始化值为’/u0000' 空格
+        //布尔类型：默认初始化值false
+        //引用数据类型：默认初始化值null
+
+
+    }
+}
